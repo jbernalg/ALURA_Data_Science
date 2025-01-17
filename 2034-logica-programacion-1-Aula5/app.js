71998fc525bf342ac5acc51cd8dd4c5701cbd777
jbernalg <jeinffersonbernal@gmail.com>
2025-01-17 10:19:46 -0500

    solucion clase

--- 2034-logica-programacion-1-Aula5/app.js
+++ 2034-logica-programacion-1-Aula5/app.js
@@ -1,36 +1,45 @@
-//Variables
+// variables
 let numeroMaximoPosible = 100;
 let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
 let numeroUsuario = 0;
 let intentos = 1;
 //let palabraVeces = 'vez';
-let maximosIntentos = 6;
+let maximosIntentos = 10;
 
+// mostrar el numero secreto por consola
+//console.log(numeroSecreto)
 
-while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = parseInt(prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible}  por favor:`));
-
+// ciclo
+while(numeroSecreto != numeroUsuario) {
+    // solicitud del numero al usuario. Convertir en entero
+    numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${numeroMaximoPosible} por favor:`));
+    // mostrar por consola el tipo de dato de numeroUsuario
     console.log(typeof(numeroUsuario));
-    if (numeroUsuario == numeroSecreto) {
-        //Acertamos, fue verdadera la condición
-        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
+
+    // codigo de comparacion
+    if (numeroSecreto == numeroUsuario) {
+        // acerta el numero
+        alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez':'veces'}`);
     } else {
         if (numeroUsuario > numeroSecreto) {
-            alert('El número secreto es menor');
-        } else {
-            alert('El número secreto es mayor');
+            // numero secreto es menor
+            alert('El numero secreto es menor');
+        } else{
+            // numero seceto es mayor
+            alert('El numero secreto es mayor');
         }
-        //Incrementamos el contador cuando no acierta
-        //intentos = intentos + 1;
-        //intentos += 1;
+        // incrementa el contador cuando no acierta
+        // intentos = intentos + 1;
+        
+        // forma abreviada del contador
+        // intentos += 1;
         intentos++;
-
         //palabraVeces = 'veces';
+
+        // condicion de parada
         if (intentos > maximosIntentos) {
-            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+            alert(`Llegaste al numero maximo de ${maximosIntentos} intentos!`);
             break;
         }
-        //La condición no se cumplió
-        //alert('Lo siento, no acertaste el número');
     }
 }
