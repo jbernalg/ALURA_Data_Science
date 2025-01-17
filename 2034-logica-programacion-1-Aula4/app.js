3de89332bacff8dfe6ba83cded48987a4d500de6
jbernalg <jeinffersonbernal@gmail.com>
2025-01-17 09:06:40 -0500

    break y operador ternario

--- 2034-logica-programacion-1-Aula4/app.js
+++ 2034-logica-programacion-1-Aula4/app.js
@@ -1,3 +1,59 @@
+// variables
+let numeroSecreto = 5;
+let numeroUsuario = 0;
+let intentos = 1;
+//let palabraVeces = 'vez';
+let maximosIntentos = 3;
+
+// ciclo
+while(numeroSecreto != numeroUsuario) {
+    // solicitud del numero al usuario
+    numeroUsuario = prompt('Me indicas un numero entre 1 y 10 por favor:');
+
+    // codigo de comparacion
+    if (numeroSecreto == numeroUsuario) {
+        // acerta el numero
+        alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez':'veces'}`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            // numero secreto es menor
+            alert('El numero secreto es menor');
+        } else{
+            // numero seceto es mayor
+            alert('El numero secreto es mayor');
+        }
+        // incrementa el contador cuando no acierta
+        // intentos = intentos + 1;
+        
+        // forma abreviada del contador
+        // intentos += 1;
+        intentos++;
+        //palabraVeces = 'veces';
+
+        // condicion de parada
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al numero maximo de ${maximosIntentos} intentos!`);
+            break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 //Variables
 let numeroSecreto = Math.floor(Math.random()*10)+1;
 let numeroUsuario = 0;
@@ -33,3 +89,4 @@ while (numeroUsuario != numeroSecreto) {
         //alert('Lo siento, no acertaste el número');
     }
 }
+*/
