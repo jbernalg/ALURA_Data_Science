61ba95105d532bbe0441e28cf7f45af9274acb85
jbernalg <jeinffersonbernal@gmail.com>
2025-01-17 10:03:23 -0500

    mostrar tipo de dato

--- 2034-logica-programacion-1-Aula4/app.js
+++ 2034-logica-programacion-1-Aula4/app.js
@@ -10,8 +10,10 @@ let maximosIntentos = 3;
 
 // ciclo
 while(numeroSecreto != numeroUsuario) {
-    // solicitud del numero al usuario
-    numeroUsuario = prompt('Me indicas un numero entre 1 y 10 por favor:');
+    // solicitud del numero al usuario. Convertir en entero
+    numeroUsuario = parseInt(prompt('Me indicas un numero entre 1 y 10 por favor:'));
+    // mostrar por consola el tipo de dato de numeroUsuario
+    console.log(typeof(numeroUsuario));
 
     // codigo de comparacion
     if (numeroSecreto == numeroUsuario) {
@@ -41,55 +43,35 @@ while(numeroSecreto != numeroUsuario) {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
-//Variables
-let numeroSecreto = Math.floor(Math.random()*10)+1;
-let numeroUsuario = 0;
-let intentos = 1;
-//let palabraVeces = 'vez';
-let maximosIntentos = 3;
+Crea un programa que utilice console.log para mostrar un mensaje de bienvenida.
 
+Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza console.log para mostrar el mensaje "¡Hola, [tu nombre]!" en la consola del navegador.
 
-while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
+Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza alert para mostrar el mensaje "¡Hola, [tu nombre]!".
 
-    console.log(typeof(numeroUsuario));
-    if (numeroUsuario == numeroSecreto) {
-        //Acertamos, fue verdadera la condición
-        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
-    } else {
-        if (numeroUsuario > numeroSecreto) {
-            alert('El número secreto es menor');
-        } else {
-            alert('El número secreto es mayor');
-        }
-        //Incrementamos el contador cuando no acierta
-        //intentos = intentos + 1;
-        //intentos += 1;
-        intentos++;
+Utiliza prompt y haz la siguiente pregunta: ¿Cuál es el lenguaje de programación que más te gusta?. Luego, almacena la respuesta en una variable y muestra la respuesta en la consola del navegador.
 
-        //palabraVeces = 'veces';
-        if (intentos > maximosIntentos) {
-            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
-            break;
-        }
-        //La condición no se cumplió
-        //alert('Lo siento, no acertaste el número');
-    }
-}
+Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. Luego, realiza la suma de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". Utiliza console.log para mostrar el mensaje "La suma de [valor1] y [valor2] es igual a [resultado]." en la consola.
+
+Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. Luego, realiza la resta de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". Utiliza console.log para mostrar el mensaje "La diferencia entre [valor1] y [valor2] es igual a [resultado]." en la consola.
+
+Pide al usuario que ingrese su edad con prompt. Con base en la edad ingresada, utiliza un if para verificar si la persona es mayor o menor de edad y muestra un mensaje apropiado en la consola.
+
+Crea una variable "numero" y solicita un valor con prompt. Luego, verifica si es positivo, negativo o cero utilizando un if-else y muestra el mensaje correspondiente.
+
+Utiliza un bucle while para mostrar los números del 1 al 10 en la consola.
+
+Crea una variable "nota" y asígnale un valor numérico. Utiliza un if-else para determinar si la nota es mayor o igual a 7 y muestra "Aprobado" o "Reprobado" en la consola.
+
+Utiliza Math.random para generar cualquier número aleatorio y muestra ese número en la consola.
+
+Utiliza Math.random para generar un número entero entre 1 y 10 y muestra ese número en la consola.
+
+Utiliza Math.random para generar un número entero entre 1 y 1000 y muestra ese número en la consola.
 */
+
+
+
+
+
