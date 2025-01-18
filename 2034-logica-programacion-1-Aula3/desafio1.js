8054fefd5be16591a35d90b9c58fad0ae6846162
jbernalg <jeinffersonbernal@gmail.com>
2025-01-18 10:05:36 -0500

    desafio 1 parte_3

---
+++ 2034-logica-programacion-1-Aula3/desafio1.js
@@ -0,0 +1,12 @@
+/*
+Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 
+'while'. Muestra cada número.
+*/
+
+let numMax = 10;
+let contador = 0;
+
+while(contador <= numMax) {
+    alert(`${contador}`);
+    contador++;
+}
