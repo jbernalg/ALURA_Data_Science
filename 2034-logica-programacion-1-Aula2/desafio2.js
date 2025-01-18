0aca5bbc47ad493e035b5c0f6137121306857a81
jbernalg <jeinffersonbernal@gmail.com>
2025-01-18 10:00:47 -0500

    desafio 5 parte_2

--- 2034-logica-programacion-1-Aula2/desafio2.js
+++ 2034-logica-programacion-1-Aula2/desafio2.js
@@ -17,9 +17,3 @@ if (numero < 0) {
 if (numero == 0) {
     alert(`El numero ${numero} es cero`);
 }
-
-/*
-Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor del saldo.
-
-Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.
-*/
---
+++ 2034-logica-programacion-1-Aula2/desafio5.js
@@ -0,0 +1,7 @@
+/*
+Pide al usuario que ingrese su nombre mediante un prompt. Luego, 
+muestra una alerta de bienvenida usando ese nombre.
+*/
+
+let nombre = prompt('Ingrese su nombre: ');
+alert(`Bienvenido al programa, ${nombre}!`);
