351a7680209b719350f2dc463560bd19381a0cb0
jbernalg <jeinffersonbernal@gmail.com>
2025-01-18 09:55:19 -0500

    desafio 3 parte_2

--- 2034-logica-programacion-1-Aula2/desafio2.js
+++ 2034-logica-programacion-1-Aula2/desafio2.js
@@ -19,8 +19,6 @@ if (numero == 0) {
 }
 
 /*
-Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, muestra "¡Felicidades, has ganado!". En caso contrario, muestra "Intentalo nuevamente para ganar.".
-
 Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor del saldo.
 
 Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.
---
+++ 2034-logica-programacion-1-Aula2/desafio3.js
@@ -0,0 +1,13 @@
+/*
+Crea un sistema de puntuación para un juego. Si la puntuación es mayor 
+o igual a 100, muestra "¡Felicidades, has ganado!". En caso contrario, 
+muestra "Intentalo nuevamente para ganar.".
+*/
+
+let puntuacion = 103;
+
+if (puntuacion>=100) {
+    alert('Felicidades! Haz ganado!');
+} else {
+    alert('Intentalo nuevamente para ganar');
+}
