72b2516050af6a942034ea973e240adc02ffe14e
jbernalg <jeinffersonbernal@gmail.com>
2025-01-18 09:58:33 -0500

    desafio 4 parte_2

---
+++ 2034-logica-programacion-1-Aula2/desafio4.js
@@ -0,0 +1,7 @@
+/*
+Crea un mensaje que informe al usuario sobre el saldo de su cuenta, 
+utilizando un template string para incluir el valor del saldo.
+*/
+
+let saldo = 2550;
+alert(`El saldo de su cuenta es de ${saldo}`);
