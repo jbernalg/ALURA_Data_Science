e8a2d19054a0aa68e7dbf3cb6a2edba4a0c4e34d
jbernalg <jeinffersonbernal@gmail.com>
2025-01-18 08:14:08 -0500

    desafio 2 parte_4

--- 2034-logica-programacion-1-Aula4/desafio1.js
+++ 2034-logica-programacion-1-Aula4/desafio1.js
@@ -3,3 +3,4 @@ Crea un programa que utilice console.log para mostrar un mensaje de
 bienvenida.
 */
 
+console.log('Bienvenidos al programa')
---
+++ 2034-logica-programacion-1-Aula4/desafio2.js
@@ -0,0 +1,8 @@
+/*
+Crea una variable llamada "nombre" y asígnale tu nombre. Luego, 
+utiliza console.log para mostrar el mensaje "¡Hola, [tu nombre]!" 
+en la consola del navegador.
+*/
+
+let nombre = 'Jeinfferson'
+console.log('Hola, ' + nombre + '!')
