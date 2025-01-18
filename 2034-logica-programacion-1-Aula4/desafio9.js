9f2f9eab5fac523fe3d378aca3aff4b7ca051ec6
jbernalg <jeinffersonbernal@gmail.com>
2025-01-18 09:12:45 -0500

    desafio 9 parte_4

---
+++ 2034-logica-programacion-1-Aula4/desafio9.js
@@ -0,0 +1,12 @@
+/*
+Utiliza un bucle while para mostrar los números del 1 al 10 en la 
+consola.
+*/
+
+let numIni = 1;
+let numFin = 10;
+
+while (numIni <= numFin) {
+    console.log(numIni);
+    numIni++;
+}
