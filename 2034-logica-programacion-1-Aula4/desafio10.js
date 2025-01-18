607cff66ed08bf1d7cec4036fdd14e700f4bb702
jbernalg <jeinffersonbernal@gmail.com>
2025-01-18 09:15:46 -0500

    desafio 10 parte_4

---
+++ 2034-logica-programacion-1-Aula4/desafio10.js
@@ -0,0 +1,13 @@
+/*
+Crea una variable "nota" y asígnale un valor numérico. Utiliza un 
+if-else para determinar si la nota es mayor o igual a 7 y muestra 
+"Aprobado" o "Reprobado" en la consola.
+*/
+
+let nota = 3;
+
+if (nota >= 7) {
+    console.log('Aprobado');
+} else {
+    console.log('Reprobado');
+}
