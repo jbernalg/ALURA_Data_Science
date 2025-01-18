7c9730c551f134b241f30b71002b1acd49e0ef20
jbernalg <jeinffersonbernal@gmail.com>
2025-01-18 08:16:52 -0500

    desafio 3 parte_4

--- 2034-logica-programacion-1-Aula4/app.js
+++ 2034-logica-programacion-1-Aula4/app.js
@@ -46,9 +46,6 @@ while(numeroSecreto != numeroUsuario) {
 /*
 
 
-Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza console.log para mostrar el mensaje "¡Hola, [tu nombre]!" en la consola del navegador.
-
-Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza alert para mostrar el mensaje "¡Hola, [tu nombre]!".
 
 Utiliza prompt y haz la siguiente pregunta: ¿Cuál es el lenguaje de programación que más te gusta?. Luego, almacena la respuesta en una variable y muestra la respuesta en la consola del navegador.
 
---
+++ 2034-logica-programacion-1-Aula4/desafio3.js
@@ -0,0 +1,7 @@
+/*
+Crea una variable llamada "nombre" y asígnale tu nombre. Luego, 
+utiliza alert para mostrar el mensaje "¡Hola, [tu nombre]!".
+*/
+
+let nombre = 'Jeinfferson'
+alert(`Hola, ${nombre}!`)
