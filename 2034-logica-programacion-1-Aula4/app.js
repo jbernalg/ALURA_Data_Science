fb313519ced355cd4d6252a36136d1650b52afdb
jbernalg <jeinffersonbernal@gmail.com>
2025-01-18 08:20:07 -0500

    desafio 4 parte_4

--- 2034-logica-programacion-1-Aula4/app.js
+++ 2034-logica-programacion-1-Aula4/app.js
@@ -45,10 +45,6 @@ while(numeroSecreto != numeroUsuario) {
 
 /*
 
-
-
-Utiliza prompt y haz la siguiente pregunta: ¿Cuál es el lenguaje de programación que más te gusta?. Luego, almacena la respuesta en una variable y muestra la respuesta en la consola del navegador.
-
 Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. Luego, realiza la suma de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". Utiliza console.log para mostrar el mensaje "La suma de [valor1] y [valor2] es igual a [resultado]." en la consola.
 
 Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. Luego, realiza la resta de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". Utiliza console.log para mostrar el mensaje "La diferencia entre [valor1] y [valor2] es igual a [resultado]." en la consola.
---
+++ 2034-logica-programacion-1-Aula4/desafio4.js
@@ -0,0 +1,8 @@
+/*
+Utiliza prompt y haz la siguiente pregunta: ¿Cuál es el lenguaje de 
+programación que más te gusta?. Luego, almacena la respuesta en una 
+variable y muestra la respuesta en la consola del navegador.
+*/
+
+let lenguaje = prompt('Cual es el lenguaje de programacion que mas te gusta?')
+console.log(lenguaje)
