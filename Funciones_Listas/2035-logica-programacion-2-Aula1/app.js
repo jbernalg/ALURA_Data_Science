07997c753e578545ceaef6d1938e99248de32574
jbernalg <jeinffersonbernal@gmail.com>
2025-01-19 09:31:05 -0500

    clase 1 parte_2

---
+++ Funciones_Listas/2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,7 @@
+// etiqueta titulo
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del numero secreto';
+
+// etiqueta parrafo
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un numero del 1 al 10';
