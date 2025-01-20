041bddffffc5363486ff7341581b4047b864af2e
jbernalg <jeinffersonbernal@gmail.com>
2025-01-20 13:59:44 -0500

    funcion generar numeros aleatorios

--- Funciones_Listas/2035-logica-programacion-2-Aula2/app.js
+++ Funciones_Listas/2035-logica-programacion-2-Aula2/app.js
@@ -1,24 +1,29 @@
-// etiqueta titulo
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML = 'Juego del numero secreto';
+// definimos variable que contiene el numero secreto
+let numeroSecreto = generarNumeroSecreto();
 
-// etiqueta parrafo
-//let parrafo = document.querySelector('p');
-//parrafo.innerHTML = 'Indica un numero del 1 al 10';
+// mostrar numero aleatorio en consola
+console.log(numeroSecreto);
 
 // funcion que asigna un texto a un elemento HTML
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 
-// funcion
+// funcion que muestra un mensaje al dar clic a un boton
 function intentoDeUsuario() {
     alert('Click desde el boton');
+    return;
+}
+
+// funcion para el numero aleatorio
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10) + 1;
 }
 
 // llamar la funcion dentro de javascript
 // etiqueta titulo
 asignarTextoElemento('h1', 'Juego Del Numero Secreto!');
 // etiqueta parrafo
-asignarTextoElemento('p', 'Indica un numero del 1 al 100: ');
+asignarTextoElemento('p', 'Indica un numero del 1 al 10: ');
