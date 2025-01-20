c9f006ae02bece9de638fc38b0b969697d6c38a4
jbernalg <jeinffersonbernal@gmail.com>
2025-01-20 15:05:08 -0500

    desafio 2

---
+++ Funciones_Listas/2035-logica-programacion-2-Aula2/desafio2.js
@@ -0,0 +1,10 @@
+/*
+Crear una función que reciba un nombre como parámetro y muestre 
+"¡Hola, [nombre]!" en la consola.
+*/
+
+function saludoNombre (nombre) {
+    console.log(`Hola ${nombre}!`)
+}
+
+saludoNombre('Jose')
