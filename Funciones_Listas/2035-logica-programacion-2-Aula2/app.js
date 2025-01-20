b63a455aa04a4a2ec5a9d26d5f5605fabeb8a5aa
jbernalg <jeinffersonbernal@gmail.com>
2025-01-20 13:47:52 -0500

    funcione con parametros

--- Funciones_Listas/2035-logica-programacion-2-Aula2/app.js
+++ Funciones_Listas/2035-logica-programacion-2-Aula2/app.js
@@ -3,13 +3,13 @@
 //titulo.innerHTML = 'Juego del numero secreto';
 
 // etiqueta parrafo
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un numero del 1 al 10';
+//let parrafo = document.querySelector('p');
+//parrafo.innerHTML = 'Indica un numero del 1 al 10';
 
 // funcion que asigna un texto a un elemento HTML
-function asignarTextoElemento() {
-    let titulo = document.querySelector('h1');
-    titulo.innerHTML = 'Juego del Numero Secreto';
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
 }
 
 // funcion
@@ -17,34 +17,8 @@ function intentoDeUsuario() {
     alert('Click desde el boton');
 }
 
-
-/*
-let numeroSecreto = generarNumeroSecreto();
-let intentos = 0;
-
-console.log(numeroSecreto);
-
-function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
-
-function verificarIntento() {
-    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    
-    if (numeroDeUsuario === numeroSecreto) {
-        console.log('Acertaste el número!');
-    } 
-    return;
-}
-
-
-function generarNumeroSecreto() {
-    return Math.floor(Math.random()*10)+1;
-
-}
-
-asignarTextoElemento('h1','Juego del número secreto!');
-asignarTextoElemento('p',`Indica un número del 1 al 10`);
-*/
+// llamar la funcion dentro de javascript
+// etiqueta titulo
+asignarTextoElemento('h1', 'Juego Del Numero Secreto!');
+// etiqueta parrafo
+asignarTextoElemento('p', 'Indica un numero del 1 al 100: ');
