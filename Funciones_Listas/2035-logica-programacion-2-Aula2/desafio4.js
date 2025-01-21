ac1b4807d2f249276c450709f5270901ac4c5812
jbernalg <jeinffersonbernal@gmail.com>
2025-01-21 07:32:07 -0500

    desafio 4

---
+++ Funciones_Listas/2035-logica-programacion-2-Aula2/desafio4.js
@@ -0,0 +1,11 @@
+/*
+Crear una función que reciba tres números como parámetros y devuelva su 
+promedio.
+*/
+
+function promedioNumeros (num1, num2, num3) {
+    promedio = (num1 + num2 + num3)/3;
+    return promedio;
+}
+// mostrar resultado por consola
+console.log(promedioNumeros(1,2,3));
