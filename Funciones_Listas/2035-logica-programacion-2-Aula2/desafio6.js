05f6495c6610e8371a55b4cb0996b775d94fc4a4
jbernalg <jeinffersonbernal@gmail.com>
2025-01-21 07:40:23 -0500

    desafio 6

---
+++ Funciones_Listas/2035-logica-programacion-2-Aula2/desafio6.js
@@ -0,0 +1,10 @@
+/*
+Crear una función que reciba un número como parámetro y devuelva el 
+resultado de multiplicar ese número por sí mismo.
+*/
+
+function numeroMultiplicado (num) {
+    return num*num;
+}
+
+console.log(numeroMultiplicado(9));
