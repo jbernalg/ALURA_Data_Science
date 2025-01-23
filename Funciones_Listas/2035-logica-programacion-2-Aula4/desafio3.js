89042004ce8546484146db88256208c04c19adfc
jbernalg <jeinffersonbernal@gmail.com>
2025-01-23 13:53:40 -0500

    desafio 3 resuelto

---
+++ Funciones_Listas/2035-logica-programacion-2-Aula4/desafio3.js
@@ -0,0 +1,9 @@
+/*
+Agrega a la lista "lenguagesDeProgramacion los siguientes elementos: 'Java', 'Ruby' y 'GoLang'.
+*/
+
+lenguajesDeProgramacion = ['Javascript', 'C', 'C++', 'Kotlin', 'Python'];
+console.log(lenguajesDeProgramacion);
+
+lenguajesDeProgramacion.push('Java', 'Ruby', 'Goland');
+console.log(lenguajesDeProgramacion);
