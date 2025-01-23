205a9648c664c5baa16d4e146a80c10f4f017b1c
jbernalg <jeinffersonbernal@gmail.com>
2025-01-23 13:50:21 -0500

    desafio 2 resuelto

---
+++ Funciones_Listas/2035-logica-programacion-2-Aula4/desafio2.js
@@ -0,0 +1,7 @@
+/*
+Crea una lista de lenguajes de programación llamada "lenguagesDeProgramacion con los siguientes elementos: 
+'JavaScript', 'C', 'C++', 'Kotlin' y 'Python'.
+*/
+
+lenguajesDeProgramacion = ['Javascript', 'C', 'C++', 'Kotlin', 'Python'];
+console.log(lenguajesDeProgramacion);
